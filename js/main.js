e41481df1a498d82ab83ccee7b69cddd4a2c017b
rhgrive2 <hataritu70@gmail.com>
2026-05-17 11:24:49 +0900

    Load BCU KBEff paraTo after knockback runtime patches

--- js/main.js
+++ js/main.js
@@ -39,6 +39,7 @@ async function boot() {
     await import('./battle/BattleSceneBcuStageBasisTickPatch.js');
     await import('./battle/BcuKnockbackRuntimePatch.js');
     await import('./battle/BcuKnockbackProcPriorityPatch.js');
+    await import('./battle/BcuKnockbackEffectLayerPatch.js');
     await import('./battle/BattleSceneAttackEffectPatch.js');
     await import('./battle/BattleSceneRendererBcuOriginPatch.js');
     await import('./battle/BattleSceneRendererHudPatch.js');

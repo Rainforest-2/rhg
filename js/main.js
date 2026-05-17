ff3d7933b531916aa4c21029a3168c85a0eb0a08
rhgrive2 <hataritu70@gmail.com>
2026-05-17 11:50:00 +0900

    Install BCU proc immunity patch

--- js/main.js
+++ js/main.js
@@ -41,6 +41,7 @@ async function boot() {
     await import('./battle/BcuKnockbackProcPriorityPatch.js');
     await import('./battle/BcuKnockbackEffectLayerPatch.js');
     await import('./battle/BcuKnockbackAnimationPatch.js');
+    await import('./battle/BcuProcImmunityPatch.js');
     await import('./battle/BattleSceneAttackEffectPatch.js');
     await import('./battle/BattleSceneRendererBcuOriginPatch.js');
     await import('./battle/BattleSceneRendererHudPatch.js');

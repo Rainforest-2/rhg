4b99ac22708a3e7214b8a90ca6ffaa9e84eeb5a0
rhgrive2 <hataritu70@gmail.com>
2026-07-03 23:47:34 +0900

    Install friendly fire capture guard

--- js/boot/groups/battleDirectPatches.js
+++ js/boot/groups/battleDirectPatches.js
@@ -1,3 +1,4 @@
 // Single source of truth for the battle "direct" touch/mobile-input boot patches (dev + prod).
+import '../../battle/BattleFriendlyFireGuardPatch.js';
 import '../../battle/BattleSceneBcuTouchPatch.js';
 import '../../battle/BattleSceneBcuMobileInputPatch.js';

ef8f61e346aa7b2feec535fbb2eecfb49a4152d7
rhgrive3 <rhgrive@gmail.com>
2026-07-23 21:45:47 +0900

    fix: install status tampering usability patch

--- js/boot/groups/uiPatches.js
+++ js/boot/groups/uiPatches.js
@@ -11,6 +11,7 @@ import '../../ui/NyankoUiBehaviorPatch.js';
 import '../../ui/ProductionCardDogIconFitPatch.js';
 import '../../ui/FormationEditorBcuUnitLevelPatch.js';
 import '../../ui/FormationCharacterModificationPatch.js';
+import '../../ui/CharacterModificationUsabilityPatch.js';
 import '../../ui/FormationCharacterTuningMobileLandscapePatch.js';
 import '../../ui/FormationCustomStageBattlePatch.js';
 import '../../ui/FormationStageDifficultyPatch.js';

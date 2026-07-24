408a895019cc294d9826f2ff2b2bd00c020b71c4
rhgrive3 <rhgrive@gmail.com>
2026-07-24 11:47:05 +0900

    fix: install BCU player capacity production gate

--- js/boot/groups/battleDirectPatches.js
+++ js/boot/groups/battleDirectPatches.js
@@ -1,4 +1,5 @@
 // Single source of truth for the battle "direct" touch/mobile-input boot patches (dev + prod).
+import '../../battle/BcuPlayerCapacityProductionPatch.js';
 import '../../battle/BattleFriendlyFireGuardPatch.js';
 import '../../battle/BattleSceneBcuTouchPatch.js';
 import '../../battle/BattleSceneBcuMobileInputPatch.js';
